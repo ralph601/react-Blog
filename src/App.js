583a76269f3366a5e376b96f5055c78391d02f21
ralph601 <jeff0101@naver.com>
2022-12-23 14:52:42 +0900

    useState/Btn function update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+
 
 function App() {
+  let post = '강남 우동 맛집';
+  let [postTitle, b] = useState(['남자 코트 추천','여자 코트 추천','남여 코트 추천']);// state는 html에 자동으로 재렌더링 가능.
+  let [rcmnd,rcmndPlus]=useState(0);//추천 버튼
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header-back">
+        <h4>개인 블로그</h4>
+      </div>
+      <div className='list'>
+        <h4>{ postTitle[0] } <span type="button" onClick={()=>{ rcmndPlus(rcmnd = rcmnd+1) }}>좋아요</span>{rcmnd}</h4>
+        <p>12월 23일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{ postTitle[1] }</h4>
+        <p>12월 23일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{ postTitle[2] }</h4>
+        <p>12월 23일 발행</p>
+      </div>
+      {/* <h4>{ post }</h4>  */}
     </div>
   );
 }
